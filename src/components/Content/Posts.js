19099815a3efcc40a2a9e7143e9a9e9fa6b76107
Veaceslav <aramexfrontend@mail.ru>
2020-02-25 22:47:08 +0200

    map method

--- src/components/Content/Posts.js
+++ src/components/Content/Posts.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import Post from './Post';
 
-let postData = [
+let posts = [
     {id:1,message:'Hello how are you',likesCount:10},
     {id:2,message:'Hello how are you to',likesCount:25},
     {id:3,message:'Happy birthday',likesCount:30},
     {id:4,message:'Hello how are you',likesCount:20},
+    {id:4,message:'Hello how are you',likesCount:20},
+    {id:4,message:'Hello how are you',likesCount:20}
 ]
+
+let post = posts.map(p => <Post message={p.message} id={p.id} likes = {p.likesCount}/>);
 
 const Posts = () => {
 
     return (
             <div className='post__wrapper'>
-                <Post message={postData[0].message} id={postData[0].id} likes = {postData[0].likesCount}/>
+
+                {post}
+                {/* <Post message={postData[0].message} id={postData[0].id} likes = {postData[0].likesCount}/>
                 <Post message={postData[1].message} id={postData[1].id} likes = {postData[1].likesCount}/>
-                <Post message={postData[2].message} id={postData[2].id} likes = {postData[2].likesCount}/>
+                <Post message={postData[2].message} id={postData[2].id} likes = {postData[2].likesCount}/> */}
                 {/* <Post message='HEY how are you?' likes = '11'/>
                 <Post message="It's my first post!!!" likes = '10'/> */}
                 
--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -4,6 +4,7 @@ import {NavLink} from 'react-router-dom';
 const DialogItem = (props) => {
 
     let path = "/dialogs/" + props.id;
+    
     return <div className="dialogs__wrapper-items active-item">
         <NavLink className="dialogs__wrapper-links" to={path}>
             {props.name}
@@ -15,43 +16,33 @@ const Message = (props) => {
     return <div className="dialogs__wrapper-messages-message">{props.message}</div>
 }
 
-let dialogsData = [
+let dialogs = [
     {id:1,name:'Olaga'},
     {id:2,name:'Vasea'},
     {id:3,name:'Oleg'},
     {id:4,name:'Misha'},
     {id:5,name:'Slava'},
 ]
-let messagesData = [
+let messages = [
     {id:1,message:'Hello how are you'},
     {id:2,message:'Hello how are you to'},
     {id:3,message:'Happy birthday'},
     {id:4,message:'Hello how are you'},
 ]
 
+let dialog = dialogs.map( d  => <DialogItem name={d.name} id={d.id}/>);
+let message = messages.map(m => <Message message={m.message} id={m.id}/>);
+
+
 const Dialogs = () => {
     return (
         <div className="dialogs__wrapper">
             <h3>Dialogs</h3>
             <div className="dialogs__wrapper-dialogs">
-
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-               {/*  <DialogItem name='Vasea' id='2'/>
-                <DialogItem name='Oleg' id='3'/>
-                <DialogItem name='Misha' id='4'/>
-                <DialogItem name='Slava' id='5'/> */}
-
+                {dialog}
             </div>
             <div className="dialogs__wrapper-messages">
-
-                <Message message={messagesData[0].message} id={messagesData[0].id}/>
-                <Message message={messagesData[1].message} id={messagesData[1].id}/>
-                <Message message={messagesData[2].message} id={messagesData[2].id}/>
-               {/*  <Mesage message="Hello how are you to "/>
-                <Mesage message="Happy birthday"/>
-                <Mesage message="Hello how are you"/> */}
-
+               {message}
             </div>
         </div>
     )
