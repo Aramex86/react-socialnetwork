926789ba69c79445bda190448fb7b1804e18ea80
Aramex86 <slavbez@mail.ru>
2020-07-22 11:56:50 +0300

    Updates

---
+++ precache-manifest.1e17f1e911239874bf7e9222817a2b14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf234acc0ef75e5b3ba80f1dfb058a22",
+    "url": "/SlavaFrEd/demo-site/index.html"
+  },
+  {
+    "revision": "2b5b4bc80c7dac498eb7",
+    "url": "/SlavaFrEd/demo-site/static/css/main.2eaf8c48.chunk.css"
+  },
+  {
+    "revision": "136e53595eee462b7e86",
+    "url": "/SlavaFrEd/demo-site/static/js/2.0a4d76a8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/SlavaFrEd/demo-site/static/js/2.0a4d76a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b68b23664162f58bac2",
+    "url": "/SlavaFrEd/demo-site/static/js/3.994a5cfc.chunk.js"
+  },
+  {
+    "revision": "6a08898061419ffb43ff",
+    "url": "/SlavaFrEd/demo-site/static/js/4.84ebfca6.chunk.js"
+  },
+  {
+    "revision": "2b5b4bc80c7dac498eb7",
+    "url": "/SlavaFrEd/demo-site/static/js/main.754b1357.chunk.js"
+  },
+  {
+    "revision": "a4bbbe88fa42e41fd8e8",
+    "url": "/SlavaFrEd/demo-site/static/js/runtime-main.91dd38a1.js"
+  },
+  {
+    "revision": "b801b1a54b7020185aded05de9ed4070",
+    "url": "/SlavaFrEd/demo-site/static/media/github.b801b1a5.png"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/SlavaFrEd/demo-site/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "f5840b790942d64c5fe51fb427d1973c",
+    "url": "/SlavaFrEd/demo-site/static/media/user.f5840b79.png"
+  }
+]);
