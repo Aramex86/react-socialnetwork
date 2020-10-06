a3e44eb320338eb33bc4bd79e3c647bbee84bfb0
Aramex86 <slavbez@mail.ru>
2020-10-06 19:56:06 +0300

    Updates

---
+++ precache-manifest.1b10d67a8aba35745817e8516949f77f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a817dea1e31ea350a130c53cd4144f0",
+    "url": "/react-socialnetwork/index.html"
+  },
+  {
+    "revision": "24cd55e53046ff3f02b5",
+    "url": "/react-socialnetwork/static/css/main.ea4ee6da.chunk.css"
+  },
+  {
+    "revision": "99a994621e65db12e895",
+    "url": "/react-socialnetwork/static/js/2.51b3a619.chunk.js"
+  },
+  {
+    "revision": "24a84c9401e1580e8b7bb987b0e971cb",
+    "url": "/react-socialnetwork/static/js/2.51b3a619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757d28869d840da55d5c",
+    "url": "/react-socialnetwork/static/js/3.2e20e619.chunk.js"
+  },
+  {
+    "revision": "0aba677d35fbc1429568",
+    "url": "/react-socialnetwork/static/js/4.5603a995.chunk.js"
+  },
+  {
+    "revision": "b3eb3e55da54c06791ef",
+    "url": "/react-socialnetwork/static/js/5.dc6b29c2.chunk.js"
+  },
+  {
+    "revision": "a1a7ddf439194ca86d83",
+    "url": "/react-socialnetwork/static/js/6.ebfe877b.chunk.js"
+  },
+  {
+    "revision": "24cd55e53046ff3f02b5",
+    "url": "/react-socialnetwork/static/js/main.c6a43916.chunk.js"
+  },
+  {
+    "revision": "baa91e9775fd894d2643",
+    "url": "/react-socialnetwork/static/js/runtime-main.043e74f5.js"
+  },
+  {
+    "revision": "450f07ada5656317b6031df338f0ac2b",
+    "url": "/react-socialnetwork/static/media/Avicii.450f07ad.png"
+  },
+  {
+    "revision": "5d857368cf52e9830a2adc899f671674",
+    "url": "/react-socialnetwork/static/media/Broken Arrows.5d857368.mp3"
+  },
+  {
+    "revision": "8df95b71df0d7193d66661cb62f3165d",
+    "url": "/react-socialnetwork/static/media/DirtFGL.8df95b71.jpg"
+  },
+  {
+    "revision": "84e565cddd8778ea09afc5e494a2595a",
+    "url": "/react-socialnetwork/static/media/Florida Georgia Line - Dirt.84e565cd.mp3"
+  },
+  {
+    "revision": "109b1406d687960ab344cc039cca0fed",
+    "url": "/react-socialnetwork/static/media/LonelyTogether.109b1406.mp3"
+  },
+  {
+    "revision": "4a8de1e78faa05b48c2ce85459c72b2c",
+    "url": "/react-socialnetwork/static/media/The Chainsmokers & Coldplay - Something Just Like This.4a8de1e7.mp3"
+  },
+  {
+    "revision": "927aa761b279b189c0d8d3a2cc87e05a",
+    "url": "/react-socialnetwork/static/media/TheDays.927aa761.mp3"
+  },
+  {
+    "revision": "c38defe33b7fe235f28abb183a879f12",
+    "url": "/react-socialnetwork/static/media/arrow.c38defe3.svg"
+  },
+  {
+    "revision": "ed97fb9fcd5fac714a66ffb8b7968855",
+    "url": "/react-socialnetwork/static/media/brokenArrows.ed97fb9f.jpg"
+  },
+  {
+    "revision": "6da21a6c7ab20116dcddeb6915b721d7",
+    "url": "/react-socialnetwork/static/media/loginPagePicture.6da21a6c.jpg"
+  },
+  {
+    "revision": "37651542f7895c0c32226c0077690afa",
+    "url": "/react-socialnetwork/static/media/ritaOra.37651542.jpg"
+  },
+  {
+    "revision": "3dd36f8cc88ab7903591a7468b9827f6",
+    "url": "/react-socialnetwork/static/media/something.3dd36f8c.jpg"
+  },
+  {
+    "revision": "f5840b790942d64c5fe51fb427d1973c",
+    "url": "/react-socialnetwork/static/media/user.f5840b79.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-socialnetwork/precache-manifest.c80e5845c5f4ad74b55795ff37235209.js"
+  "/react-socialnetwork/precache-manifest.1b10d67a8aba35745817e8516949f77f.js"
 );
 
 self.addEventListener('message', (event) => {
